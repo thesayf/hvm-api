853685089b0767d27f4b7effc904288c29b32ac4
Rudi Hinds <rudihinds@gmail.com>
2021-09-11 11:31:44 +0100

    added function to extract city living data for one city

---
+++ utils/getCitiesData.js
@@ -0,0 +1,31 @@
+var axios = require("axios").default;
+const fs = require("fs");
+const fetch = require("node-fetch");
+
+// parse city data to js obj
+// each city obj, get city name
+// create query str
+// fetch city data
+// store in DB
+
+let queryStr =
+  "https://www.numbeo.com/api/city_prices?api_key=1dap9z2dq3eguv&query=";
+
+const parseCityData = async () => {
+  fs.readFile("./cities+5m.json", "utf8", (err, data) => {
+    if (err) console.log(err);
+    const cityName = JSON.parse(data)[0].name;
+    queryStr = queryStr + cityName;
+    fetch(queryStr)
+      .then((res) => res.json())
+      .then(data => data)
+      .catch(err => console.log('error getting city data', err));
+    // console.log(queryStr);
+  });
+};
+
+console.log(parseCityData());
+
+const getCitiesData = () => {};
+
+module.exports = getCitiesData;
