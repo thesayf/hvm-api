5a58d49f75e6fea53393a09e80ce4574efb9eb8a
Rudi Hinds <rudihinds@Rudis-MacBook-Pro.local>
2023-04-16 18:35:07 +0100

    create CountriesPrices table and add price data

--- lambdas/dataIngestion/writeCountries.js
+++ lambdas/dataIngestion/writeCountries.js
@@ -18,32 +18,13 @@ const baseCurrency = "USD";
 
 const createTableParams = {
   TableName: "CountriesPrices",
-  KeySchema: [{ AttributeName: "cityId", KeyType: "HASH" }],
-  AttributeDefinitions: [{ AttributeName: "cityId", AttributeType: "N" }],
+  KeySchema: [{ AttributeName: "countryName", KeyType: "HASH" }],
+  AttributeDefinitions: [{ AttributeName: "countryName", AttributeType: "S" }],
   ProvisionedThroughput: {
     // will need to be considered for launch, or move to pay as you go to get an idea first three months.
     ReadCapacityUnits: 5,
     WriteCapacityUnits: 5,
   },
-};
-
-const data = {
-  name: "United States",
-  currency: "USD",
-  contributors12months: 12659,
-  monthLastUpdate: 4,
-  contributors: 24748,
-  yearLastUpdate: 2023,
-  prices: [
-    {
-      data_points: 2158,
-      item_id: 1,
-      lowest_price: 11,
-      average_price: 20,
-      highest_price: 35,
-      item_name: "Meal, Inexpensive Restaurant, Restaurants",
-    },
-  ],
 };
 
 const jsonFilePath = path.resolve(
@@ -59,15 +40,11 @@ const sema = new Sema(5); // Limit concurrency to 5 requests at a time
 // var xRatesUrl = "https://api.exchangerate.host/latest";
 
 const getBaseExchangeRate = async (fromCurrency) => {
-  console.log("from", fromCurrency);
   var xRatesUrl = `https://api.exchangerate.host/convert?from=${fromCurrency}&to=${baseCurrency}`;
-  console.log('url', xRatesUrl);
   try {
     const res = await fetch(xRatesUrl);
     const data = await res.json();
     if (data.success) {
-      console.log('data', data);
-      console.log("result", data.result);
       return data.result;
     } else {
       throw Error(`error making api call to convert ${fromCurrency} to USD`);
@@ -77,85 +54,116 @@ const getBaseExchangeRate = async (fromCurrency) => {
   }
 };
 
-fs.readFile(jsonFilePath, null, async (err, data) => {
-  if (err) console.log(err);
-  const countries = JSON.parse(data).supported_countries;
+const insertCountryData = (countryObj) => {
+  const {
+    currency,
+    name,
+    contributors12months,
+    monthLastUpdate,
+    contributors,
+    yearLastUpdate,
+    prices,
+    usdPrices,
+  } = countryObj;
 
-  try {
-    for (const country of countries) {
-      await sema.acquire();
+  const putParams = {
+    TableName: "CountriesPrices",
+    Item: {
+      countryName: name,
+      currency,
+      contributors12months,
+      monthLastUpdate,
+      contributors,
+      yearLastUpdate,
+      prices,
+      usdPrices,
+    },
+  };
 
-      try {
-        const res = await fetch(
-          `https://www.numbeo.com/api/country_prices?api_key=z8dasdlhhu49hk&country=${country}`
-        );
-
-        if (res.ok) {
-          const priceObj = await res.json();
-          const baseExchangeRate = await getBaseExchangeRate(priceObj.currency);
-          console.log("base x rate", baseExchangeRate);
-          const newPriceObj = {
-            ...priceObj,
-            usdPrices: priceObj.prices.map((itemPrice) => {
-              return {
-                ...itemPrice,
-                lowest_price: itemPrice.lowest_price * baseExchangeRate,
-                average_price: itemPrice.average_price * baseExchangeRate,
-                highest_price: itemPrice.highest_price * baseExchangeRate,
-              };
-            }),
-          };
-          console.log(newPriceObj);
-        }
-      } catch (err) {
-        console.log(err);
-        break;
-      } finally {
-        sema.release();
-      }
+  docClient.put(putParams, (err, data) => {
+    if (err) {
+      console.error(
+        `Unable to add country: ${name}`,
+        JSON.stringify(err, null, 2)
+      );
+    } else {
+      console.log("Added city:", JSON.stringify(data, null, 2));
     }
-  } catch (err) {
-    console.log("Breaking out of the loop: ", err.message);
+  });
+};
+
+// create country item with same attributes but add a usdprices field which uses exchange rate to get prices converted to usd.
+const createPricesObjectAndWriteToDb = () => {
+  fs.readFile(jsonFilePath, null, async (err, data) => {
+    if (err) console.log(err);
+    const countries = JSON.parse(data).supported_countries;
+
+    try {
+      for (const country of countries) {
+        await sema.acquire();
+        try {
+          const res = await fetch(
+            `https://www.numbeo.com/api/country_prices?api_key=z8dasdlhhu49hk&country=${country}`
+          );
+
+          if (res.ok) {
+            const priceObj = await res.json();
+            const baseExchangeRate = await getBaseExchangeRate(
+              priceObj.currency
+            );
+            const countryPriceObj = {
+              ...priceObj,
+              usdPrices: priceObj.prices.map((itemPrice) => {
+                if (itemPrice.lowest_price) {
+                  itemPrice.lowest_price =
+                    itemPrice.lowest_price * baseExchangeRate;
+                }
+                if (itemPrice.highest_price) {
+                  itemPrice.highest_price =
+                    itemPrice.highest_price * baseExchangeRate;
+                }
+                if (itemPrice.average_price) {
+                  itemPrice.average_price =
+                    itemPrice.average_price * baseExchangeRate;
+                }
+                return itemPrice
+              }),
+            };
+            insertCountryData(countryPriceObj);
+          }
+        } catch (err) {
+          console.log(err);
+          break;
+        } finally {
+          sema.release();
+        }
+      }
+    } catch (err) {
+      console.error(err);
+    }
+  });
+};
+
+dynamoDB.describeTable({ TableName: "CountriesPrices" }, (err, data) => {
+  if (err) {
+    if (err.code === "ResourceNotFoundException") {
+      // Table does not exist, create it
+      dynamoDB.createTable(createTableParams, (err, data) => {
+        if (err) {
+          console.error(
+            "Unable to create table:",
+            JSON.stringify(err, null, 2)
+          );
+        } else {
+          console.log("Created table:", JSON.stringify(data, null, 2));
+          createPricesObjectAndWriteToDb();
+        }
+      });
+    } else {
+      console.error("Unable to describe table:", JSON.stringify(err, null, 2));
+    }
+  } else {
+    console.log("Table already exists:", JSON.stringify(data, null, 2));
+    createPricesObjectAndWriteToDb();
   }
 });
-
-// create country item with same attributes but add a usd field which uses exchange rate.
-// write usdPrices array which is a mapped prices array which applies exchange rate for every field (check always the same)
-
-// fs.readFile(jsonFilePath, null, async (err, data) => {
-//   if (err) console.log(err);
-//   const countries = JSON.parse(data).supported_countries;
-//   countries.forEach(async (country) => {
-//     try {
-//       await sema.acquire();
-//       const res = await fetch(
-//         `https://www.numbeo.com/api/country_prices?api_key=z8dasdlhhu49hk&country=${country}`
-//       );
-//       if (res.ok) {
-//         const priceObj = await res.json();
-//         const baseExchangeRate = await getBaseExchangeRate(priceObj.currency);
-//         console.log("base x rate", baseExchangeRate);
-//         const newPriceObj = {
-//           ...priceObj,
-//           usdPrices: priceObj.prices.map((itemPrice) => {
-//             return {
-//               ...itemPrice,
-//               lowest_price: itemPrice.lowest_price / baseExchangeRate,
-//               average_price: itemPrice.average_price / baseExchangeRate,
-//               highest_price: itemPrice.highest_price / baseExchangeRate,
-//             };
-//           }),
-//         };
-//         console.log(newPriceObj);
-//         throw Error('stop')
-//       }
-//     } catch (err) {
-//       console.log(err);
-//       break;
-//     } finally {
-//       sema.release();
-//     }
-//   });
-// });
-
-// need to write the countries table with partition key of country_name no sort key. will need to add gsi on cities for country lookup
