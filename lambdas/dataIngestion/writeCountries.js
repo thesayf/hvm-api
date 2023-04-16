212658d43764689a189a9a5e233680dd3bbc1823
Rudi Hinds <rudihinds@Rudis-MacBook-Pro.local>
2023-04-16 03:11:43 +0100

    get exchange rates for usd for each country, create usd prices array for our db

--- lambdas/dataIngestion/writeCountries.js
+++ lambdas/dataIngestion/writeCountries.js
@@ -3,8 +3,6 @@ const path = require("path");
 const { Sema } = require("async-sema");
 const AWS = require("aws-sdk");
 
-// https://www.npmjs.com/package/currency-exchange-rate uses yahoo finance
-
 AWS.config.update({
   region: "eu-west-1",
   accessKeyId: "fakeaccess",
@@ -14,6 +12,7 @@ AWS.config.update({
 
 const dynamoDB = new AWS.DynamoDB();
 const docClient = new AWS.DynamoDB.DocumentClient();
+const baseCurrency = "USD";
 
 // create cities table, and add all cities' data from cities+5m.json
 
@@ -28,6 +27,25 @@ const createTableParams = {
   },
 };
 
+const data = {
+  name: "United States",
+  currency: "USD",
+  contributors12months: 12659,
+  monthLastUpdate: 4,
+  contributors: 24748,
+  yearLastUpdate: 2023,
+  prices: [
+    {
+      data_points: 2158,
+      item_id: 1,
+      lowest_price: 11,
+      average_price: 20,
+      highest_price: 35,
+      item_name: "Meal, Inexpensive Restaurant, Restaurants",
+    },
+  ],
+};
+
 const jsonFilePath = path.resolve(
   __dirname,
   "..",
@@ -38,17 +56,68 @@ const jsonFilePath = path.resolve(
 
 const sema = new Sema(5); // Limit concurrency to 5 requests at a time
 
-var requestURL = "https://api.exchangerate.host/latest";
-// convert var requestURL = "https://api.exchangerate.host/convert?from=USD&to=EUR"; 
+// var xRatesUrl = "https://api.exchangerate.host/latest";
 
+const getBaseExchangeRate = async (fromCurrency) => {
+  console.log("from", fromCurrency);
+  var xRatesUrl = `https://api.exchangerate.host/convert?from=${fromCurrency}&to=${baseCurrency}`;
+  console.log('url', xRatesUrl);
+  try {
+    const res = await fetch(xRatesUrl);
+    const data = await res.json();
+    if (data.success) {
+      console.log('data', data);
+      console.log("result", data.result);
+      return data.result;
+    } else {
+      throw Error(`error making api call to convert ${fromCurrency} to USD`);
+    }
+  } catch (err) {
+    console.error(err);
+  }
+};
 
-const getExchangeRates = async () => {
-   const res = await fetch(requestURL)
-   const data = await res.json()
-   console.log(data);
-}
+fs.readFile(jsonFilePath, null, async (err, data) => {
+  if (err) console.log(err);
+  const countries = JSON.parse(data).supported_countries;
 
-getExchangeRates()
+  try {
+    for (const country of countries) {
+      await sema.acquire();
+
+      try {
+        const res = await fetch(
+          `https://www.numbeo.com/api/country_prices?api_key=z8dasdlhhu49hk&country=${country}`
+        );
+
+        if (res.ok) {
+          const priceObj = await res.json();
+          const baseExchangeRate = await getBaseExchangeRate(priceObj.currency);
+          console.log("base x rate", baseExchangeRate);
+          const newPriceObj = {
+            ...priceObj,
+            usdPrices: priceObj.prices.map((itemPrice) => {
+              return {
+                ...itemPrice,
+                lowest_price: itemPrice.lowest_price * baseExchangeRate,
+                average_price: itemPrice.average_price * baseExchangeRate,
+                highest_price: itemPrice.highest_price * baseExchangeRate,
+              };
+            }),
+          };
+          console.log(newPriceObj);
+        }
+      } catch (err) {
+        console.log(err);
+        break;
+      } finally {
+        sema.release();
+      }
+    }
+  } catch (err) {
+    console.log("Breaking out of the loop: ", err.message);
+  }
+});
 
 // create country item with same attributes but add a usd field which uses exchange rate.
 // write usdPrices array which is a mapped prices array which applies exchange rate for every field (check always the same)
@@ -63,35 +132,30 @@ getExchangeRates()
 //         `https://www.numbeo.com/api/country_prices?api_key=z8dasdlhhu49hk&country=${country}`
 //       );
 //       if (res.ok) {
-//         const prices = await res.json();
-//         console.log(prices);
+//         const priceObj = await res.json();
+//         const baseExchangeRate = await getBaseExchangeRate(priceObj.currency);
+//         console.log("base x rate", baseExchangeRate);
+//         const newPriceObj = {
+//           ...priceObj,
+//           usdPrices: priceObj.prices.map((itemPrice) => {
+//             return {
+//               ...itemPrice,
+//               lowest_price: itemPrice.lowest_price / baseExchangeRate,
+//               average_price: itemPrice.average_price / baseExchangeRate,
+//               highest_price: itemPrice.highest_price / baseExchangeRate,
+//             };
+//           }),
+//         };
+//         console.log(newPriceObj);
 //         throw Error('stop')
 //       }
 //     } catch (err) {
 //       console.log(err);
+//       break;
 //     } finally {
 //       sema.release();
 //     }
 //   });
 // });
-
-const data = {
-  name: 'United States',
-  currency: 'USD',
-  contributors12months: 12659,
-  monthLastUpdate: 4,
-  contributors: 24748,
-  yearLastUpdate: 2023,
-  prices: [
-    {
-      data_points: 2158,
-      item_id: 1,
-      lowest_price: 11,
-      average_price: 20,
-      highest_price: 35,
-      item_name: 'Meal, Inexpensive Restaurant, Restaurants'
-    },
-]
-}
 
 // need to write the countries table with partition key of country_name no sort key. will need to add gsi on cities for country lookup
